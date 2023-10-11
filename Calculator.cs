26b15a56d2036ffe2801b134422ba551e5b72200
Ewan Taylor <1403182@uad.ac.uk>
2023-10-11 12:28:33 +0100

    amended calculator

---
+++ Calculator.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace UtilityKnife.Calculator
+{
+	public static class Calculator
+	{
+		public static double Add(double a, double b)
+		{
+			return a + b;
+		}
+		public static double Subtract(double a, double b)
+		{
+			return a - b;
+		}
+		public static double Multiply(double a, double b)
+		{
+			return a * b;
+		}
+		public static double Division(double a, double b)
+		{
+			return a / b;
+		}
+		public static int division(int a, int b)
+		{
+			return a / b;
+		}
+	}
+}
+
+public class Class1
+{
+	public Class1()
+	{
+	}
+}
