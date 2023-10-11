9c255f9297a59fb9c8124c833f2952b05571df06
Ewan Taylor <1403182@uad.ac.uk>
2023-10-11 12:36:44 +0100

    amended fahrenheit to celsius converter

--- Converters.cs
+++ Converters.cs
@@ -6,11 +6,12 @@ namespace UtilityKnife.Converters
     {
         public double FahrenheitToCelsius(double FahrenheitTemp)
         {
-            double _fahrenheit = 0.0;
             double _celsius = 0.0;
+            // (F – 32) * 5/9
+            var step1 = Calculator.Calculator.Subtract(fahrenheitTemp, 32);
+            var step2 = Calculator.Calculator.Multiplication(step1, 5.0);
+            _celsius = Calculator.Calculator.Division(step2, 9.0);
             return _celsius;
-            
-            // return (fahrenheit - 32) * 5 / 9;
         }
         static void Main(string[] args)
 		{
