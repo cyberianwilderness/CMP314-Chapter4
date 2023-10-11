a63a1d3bbb2fbac217dd8fb4a96dd3b52897cec5
Ewan Taylor <1403182@uad.ac.uk>
2023-10-11 11:57:01 +0100

    made class 1

---
+++ Class1.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace UtilityKnife
+{
+    public static class  Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            // skeleton program
+        }
+    }
+}
